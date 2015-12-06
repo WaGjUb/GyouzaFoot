13e115553e533c3909ac450edb528aa97249f8b4
hydrocat <vdrbandeiras@gmail.com>
2015-12-05 23:44:51 -0200

    Implementado Completamente

--- Gyouzafoot/src/gyouzafoot/DAOs/FaltaCometidaDAO.java
+++ Gyouzafoot/src/gyouzafoot/DAOs/FaltaCometidaDAO.java
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2015 wagjub
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,22 +17,102 @@
 package gyouzafoot.DAOs;
 
 import java.sql.*;
-import gyouzafoot.Objetos.FaltaCometida;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import gyouzafoot.Objetos.Falta;
+import java.util.ArrayList;
 /**
  *
  * @author wagjub
  */
 public class FaltaCometidaDAO {
         
-    Connection c;
+    Connection con;
     Conexao helper;
       
     public FaltaCometidaDAO(CredenciaisConexao cc) throws SQLException
     {
         this.helper = new Conexao();
-        this.c = helper.getConnection(cc);
+        this.con = helper.getConnection(cc);
     }
+    
+     public boolean inserir(Falta falta) throws SQLException {
+        String sql = "insert into falta_cometidas (" +
+                " id_participacao,"+
+                "gravidade )" +
+                "values( ?,? )";
+
+        PreparedStatement s = this.con.prepareStatement(sql);
+        
+        s.setInt(1, falta.getIdParticipacao());
+        s.setInt(2, falta.getGravidade());
+        
+        s.executeUpdate();
+
+        helper.closeAllConnections(con, s);
+        return true;
+
+    }
+
+    public boolean remover(Falta falta) throws SQLException {
+        String sql = "delete from falta_cometidas where id = ?";
+
+        PreparedStatement s = this.con.prepareStatement(sql);
+        s.setInt(1, falta.getId());
+        s.executeQuery();
+
+        helper.closeAllConnections(con, s);
+        return true;
+
+    }
+
+    public boolean alterar(Falta falta) throws SQLException {
+        String sql = "update faltas_cometidas set id_participacao=? gravidade=? where id = ?";
+        PreparedStatement s = this.con.prepareStatement(sql);
+        
+        s.setInt(1, falta.getIdParticipacao());
+        s.setInt(5, falta.getGravidade());
+        s.executeQuery();
+        
+        helper.closeAllConnections(con, s);
+        return true;
+
+    }
+
+    public Falta buscar(int id) throws SQLException {
+        String sql = "select * from faltas_cometidas where id = ?";
+        
+        Falta falta = null;
+        PreparedStatement s = this.con.prepareStatement(sql);
+        s.setInt(1, id);
+        ResultSet rs = s.executeQuery();
+
+        while (rs.next()) {
+            falta = new Falta(rs.getInt("id"),rs.getInt("id_participacao"),rs.getInt("gravidade"));
+        }
+
+        helper.closeAllConnections(rs, con, s);
+        return falta;
+    }
+    
+    public ArrayList<Falta> getList() throws SQLException
+    {
+        ArrayList<Falta> faltas = new ArrayList<>();
+        String sql = "select * from falta";
+        
+        PreparedStatement ps = this.con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        
+        while( rs.next() )
+        {
+            faltas.add(
+            new Falta(rs.getInt("id"),rs.getInt("id_participacao"),rs.getInt("gravidade"))
+            );
+        }
+        
+        helper.closeAllConnections(rs, con, ps);
+        return faltas;
+    }
+
+    
+    
+    
 }
