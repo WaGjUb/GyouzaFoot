5830cdcf4c14a32f216ca45a6af047b198217db9
hydrocat <vdrbandeiras@gmail.com>
2015-12-06 00:02:19 -0200

    Implementação interga

--- Gyouzafoot/src/gyouzafoot/DAOs/ParticipacaoDAO.java
+++ Gyouzafoot/src/gyouzafoot/DAOs/ParticipacaoDAO.java
@@ -19,8 +19,7 @@ package gyouzafoot.DAOs;
 
 import java.sql.*;
 import gyouzafoot.Objetos.Participacao;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 /**
  *
@@ -28,12 +27,110 @@ import java.util.logging.Logger;
  */
 public class ParticipacaoDAO {
  
-    Connection c;
+    Connection conexao;
     Conexao helper;
       
     public ParticipacaoDAO(CredenciaisConexao cc) throws SQLException
     {
         this.helper = new Conexao();
-        this.c = helper.getConnection(cc);
+        this.conexao = helper.getConnection(cc);
     }
+    
+         public boolean inserir(Participacao participacao) throws SQLException {
+        String sql = "insert into participacao (" +
+                "id_jogador,"+
+                "id_jogo,"+
+                "gol_contra,"+
+                "gol_valido"+
+                "values( ?,?,?,? )";
+
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        
+        s.setInt(1, participacao.getIdJogador());
+        s.setInt(2, participacao.getIdJogo());
+        s.setInt(3, participacao.getQuantidadeGols());
+        s.setInt(4, participacao.getQuantidadeGolsContra());
+        
+        s.executeUpdate();
+
+        helper.closeAllConnections(conexao, s);
+        return true;
+
+    }
+
+    public boolean remover(Participacao participacao) throws SQLException {
+        String sql = "delete from participacao where id = ?";
+
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        s.setInt(1, participacao.getId());
+        s.executeQuery();
+
+        helper.closeAllConnections(conexao, s);
+        return true;
+
+    }
+
+    public boolean alterar(Participacao participacao) throws SQLException {
+        String sql = "update participacao set id_jogador=? id_jogo=? gol_contra=? gol_valido=? where id = ?";
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        
+        s.setInt(1, participacao.getIdJogador());
+        s.setInt(2, participacao.getIdJogo());
+        s.setInt(3, participacao.getQuantidadeGols());
+        s.setInt(4, participacao.getQuantidadeGolsContra());
+        s.setInt(5, participacao.getId());
+        s.executeQuery();
+        
+        helper.closeAllConnections(conexao, s);
+        return true;
+
+    }
+
+    public Participacao buscar(int id) throws SQLException {
+        String sql = "select * from participacao where id = ?";
+        
+        Participacao participacao = null;
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        s.setInt(1, id);
+        ResultSet rs = s.executeQuery();
+
+        while (rs.next()) {
+            participacao = new Participacao(
+                    rs.getInt("id"),
+                    rs.getInt("id_jogador"),
+                    rs.getInt("id_jogo"),
+                    rs.getInt("gol_contra"),
+                    rs.getInt("gol_valido")
+            );
+        }
+
+        helper.closeAllConnections(rs, conexao, s);
+        return participacao;
+    }
+    
+    public ArrayList<Participacao> getList() throws SQLException
+    {
+        ArrayList<Participacao> participacoes = new ArrayList<>();
+        String sql = "select * from participacao";
+        
+        PreparedStatement ps = this.conexao.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        
+        while( rs.next() )
+        {
+            participacoes.add(
+            new Participacao(
+                    rs.getInt("id"),
+                    rs.getInt("id_jogador"),
+                    rs.getInt("id_jogo"),
+                    rs.getInt("gol_contra"),
+                    rs.getInt("gol_valido")
+            )
+            );
+        }
+        
+        helper.closeAllConnections(rs, conexao, ps);
+        return participacoes;
+    }
+
 }
