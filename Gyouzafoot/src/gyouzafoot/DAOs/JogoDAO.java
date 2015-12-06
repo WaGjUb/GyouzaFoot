fec296c5f00cfa03c65ee946a2c89d6d3c8edcc4
hydrocat <vdrbandeiras@gmail.com>
2015-12-05 23:19:28 -0200

    Implementado Completamente

--- Gyouzafoot/src/gyouzafoot/DAOs/JogoDAO.java
+++ Gyouzafoot/src/gyouzafoot/DAOs/JogoDAO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 wagjub
+ * Copyright (conexao) 2015 wagjub
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,8 +19,7 @@ package gyouzafoot.DAOs;
 
 import java.sql.*;
 import gyouzafoot.Objetos.Jogo;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 /**
  *
@@ -30,16 +29,105 @@ import java.util.logging.Logger;
 
 public class JogoDAO {
     
-    Connection c;
+    Connection conexao;
     Conexao helper;
       
     public JogoDAO(CredenciaisConexao cc) throws SQLException
     {
-<<<<<<< HEAD
-        this.c = new AssistenteConexao().GeradorConexao(cc);
-=======
         this.helper = new Conexao();
-        this.c = helper.getConnection(cc);
->>>>>>> bba5ab66ca16566f5c9d23a64b4aefa187259db6
+        this.conexao = helper.getConnection(cc);
     }
+    
+             public boolean inserir(Jogo jogo) throws SQLException {
+        String sql = "insert into Jogo (" +
+                "nome_adversario,"+
+                "PTgyouza,"+
+                "PTadversario,"+
+                "data)"+
+                "values( ?,?,?,? )";
+
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        
+        s.setString(1, jogo.getNomeAdversario());
+        s.setInt(2, jogo.getPontuacaoTime());
+        s.setInt(3, jogo.getPontuacaoTimeAdversario());
+        s.setDate(4, jogo.getData());
+        
+        s.executeUpdate();
+        helper.closeAllConnections(conexao, s);
+        return true;
+
+    }
+
+    public boolean remover(Jogo jogo) throws SQLException {
+        String sql = "delete from Jogo where id = ?";
+
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        s.setInt(1, jogo.getId());
+        s.executeQuery();
+
+        helper.closeAllConnections(conexao, s);
+        return true;
+
+    }
+
+    public boolean alterar(Jogo jogo) throws SQLException {
+        String sql = "update Jogo set nome_adversario=? PTgyouza=? PTadversario=? data=? where id = ?";
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        
+        s.setString(1, jogo.getNomeAdversario());
+        s.setInt(2, jogo.getPontuacaoTime());
+        s.setInt(3, jogo.getPontuacaoTimeAdversario());
+        s.setDate(4, jogo.getData());
+        s.setInt(5, jogo.getId());
+        s.executeQuery();
+        
+        helper.closeAllConnections(conexao, s);
+        return true;
+
+    }
+
+    public Jogo buscar(int id) throws SQLException {
+        String sql = "select * from Jogo where id = ?";
+        
+        Jogo jogo = null;
+        PreparedStatement s = this.conexao.prepareStatement(sql);
+        s.setInt(1, id);
+        ResultSet rs = s.executeQuery();
+
+        while (rs.next()) {
+            jogo = new Jogo(rs.getString("nome_adversario"),
+                    rs.getInt("PTgyouza"),
+                    rs.getInt("PTadversario"),
+                    rs.getDate("data")
+            );
+        }
+
+        helper.closeAllConnections(rs, conexao, s);
+        return jogo;
+    }
+    
+    public ArrayList<Jogo> getList() throws SQLException
+    {
+        ArrayList<Jogo> jogadors = new ArrayList<>();
+        String sql = "select * from Jogo";
+        
+        PreparedStatement ps = this.conexao.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        
+        while( rs.next() )
+        {
+            jogadors.add(
+                new Jogo(rs.getString("nome_adversario"),
+                    rs.getInt("PTgyouza"),
+                    rs.getInt("PTadversario"),
+                    rs.getDate("data")
+            )
+            );
+        }
+        
+        helper.closeAllConnections(rs, conexao, ps);
+        return jogadors;
+    }
+
 }
